28464bf64b74cc0bd7f241babe2a5675894b1c06
user <82736724+mthsgimenez@users.noreply.github.com>
2025-01-21 17:13:49 -0300

    Add arkenfox user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,16 @@
+// SAFE BROWSING (SB)
+user_pref("browser.safebrowsing.malware.enabled", true);
+user_pref("browser.safebrowsing.phishing.enabled", true);
+user_pref("browser.safebrowsing.downloads.enabled", true);
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);
+user_pref("browser.safebrowsing.allowOverride", true);
+
+// LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS
+user_pref("browser.search.separatePrivateDefault", false);
+user_pref("browser.search.separatePrivateDefault.ui.enabled", false);
+
+// OPTIONAL RFP (resistFingerprinting)
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.resistFingerprinting.letterboxing", true);
+user_pref("webgl.disabled", true);
+user_pref("privacy.spoof_english", 2);
