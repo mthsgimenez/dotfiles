12f723fc961c75bb797042b918cf91ac59b70e75
user <82736724+mthsgimenez@users.noreply.github.com>
2025-01-23 13:31:40 -0300

    Add waybar config

---
+++ waybar/playerctl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PLAYER="spotify"
+PREV_OUTPUT=""
+
+while true; do
+        if MUSIC=$(playerctl -s -p $PLAYER metadata -f "{{trunc(artist, 20)}} - {{trunc(title, 30)}}"); then
+                if [[ $(playerctl -s -p $PLAYER status) == "Paused" ]]; then
+                        MUSIC="*Paused*"
+                fi
+        else
+                MUSIC="No music playing"
+        fi
+
+        if [ "$PREV_OUTPUT" != "$MUSIC" ]; then
+                echo $MUSIC
+                PREV_OUTPUT="$MUSIC"
+        fi
+
+        sleep 1
+done
