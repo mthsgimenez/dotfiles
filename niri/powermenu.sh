0ccca7eab062b202d8e516aa2c7e9cd4270dcfc8
user <82736724+mthsgimenez@users.noreply.github.com>
2025-02-07 22:24:10 -0300

    Fix command

--- niri/powermenu.sh
+++ niri/powermenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 case "$(echo -e " Desligar\n Reiniciar\n Suspender\n󰩈 Sair" | fuzzel -d --index -f "RobotoMono Nerd Font:size=18" -l 4 -w 13)" in
-        "0") exec systemctl shutdown;;
+        "0") exec systemctl poweroff;;
         "1") exec systemctl reboot;;
         "2") exec systemctl suspend;;
         "3") exec niri msg action quit -s;;
