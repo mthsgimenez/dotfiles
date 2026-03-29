42669f834d6b504348bc3f5680f33d618427f9fa
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-03-29 14:08:30 -0300

    revert: undo changes for voidlinux

--- niri/powermenu.sh
+++ niri/powermenu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 case "$(echo -e " Desligar\n Reiniciar\n Suspender\n󰩈 Sair" | fuzzel -d --index -f "RobotoMono Nerd Font:size=18" -l 4 -w 13)" in
-        "0") exec loginctl poweroff;;
-        "1") exec loginctl reboot;;
-        "2") exec loginctl suspend;;
+        "0") exec systemctl poweroff;;
+        "1") exec systemctl reboot;;
+        "2") exec systemctl suspend;;
         "3") exec niri msg action quit -s;;
 esac
